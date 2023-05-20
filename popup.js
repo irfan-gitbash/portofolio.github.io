37c73fa3a471076d83b386c564ea08527f39b6c0
irfan-gitbash <suhermanirfan23@gmail.com>
2023-05-20 20:32:51 +0700

    my project

---
+++ popup.js
@@ -0,0 +1,14 @@
+alert("anda Yakin Mau Masuk ?");
+var masuk = true;
+while(masuk === true){
+    var name = prompt("Siapa Nama Anda ?");
+    alert(`hello ${name}`);
+
+    name = prompt("Dimanakah anda tinggal ?");
+    alert(`${name}`);
+
+    masuk = prompt("brapa umur anda ?");
+    masuk = prompt("apakah anda sudah punya pacar?");
+} 
+
+alert(`Terimakasih semoga anda panjang umur hihi....`);
